4624d3087d20859d99f08edfd9453ebe2e1b95ff
chenwenxing <42410334@qq.com>
2020-03-08 14:21:40 +0800

    学会了git之后，将我自己设计的网页上传到GitHub上。

---
+++ 网页设计/js/team.js
@@ -0,0 +1,263 @@
+var data=[];
+
+var dataStr='勇敢者游戏2! 勇敢者游戏2<br>\
+<br>\
+导演: 杰克·卡斯丹<br>\
+编剧: 杰夫·皮克纳 / 斯科特·罗森伯格<br>\
+主演: 道恩·强森 / 凯伦·吉兰 / 杰克·布莱克 / 凯文·哈特 / 尼克·乔纳斯 / 更多...<br>\
+类型: 喜剧 / 动作 / 奇幻 / 冒险<br>\
+制片国家/地区: 美国<br>\
+语言: 英语<br>\
+上映日期: 2019-12-06(中国大陆) / 2019-12-13(美国)<br>\
+<br>\
+<br>\
+碟中谍6! 碟中谍6<br>\
+<br>\
+导演: 克里斯托弗·麦奎里<br>\
+编剧: 克里斯托弗·麦奎里 / 布鲁斯·盖勒<br>\
+主演: 汤姆·克鲁斯 / 亨利·卡维尔 / 文·瑞姆斯 / 西蒙·佩吉 / 丽贝卡·弗格森 / 更多...<br>\
+类型: 动作 / 惊悚 / 冒险<br>\
+制片国家/地区: 美国 / 中国大陆 / 法国 / 挪威<br>\
+语言: 英语 / 法语<br>\
+上映日期: 2018-08-31(中国大陆) / 2018-07-27(美国)<br>\
+<br>\
+<br>\
+速度与激情7! 速度与激情7<br>\
+<br>\
+导演: 温子仁<br>\
+编剧: 克里斯·摩根 / 盖瑞·斯科特·汤普森<br>\
+主演: 范·迪塞尔 / 保罗·沃克 / 杰森·斯坦森 / 米歇尔·罗德里格兹 / 乔丹娜·布鲁斯特 / 更多...<br>\
+类型: 动作 / 犯罪<br>\
+制片国家/地区: 美国 / 中国大陆 / 日本<br>\
+语言: 英语<br>\
+上映日期: 2015-04-12(中国大陆) / 2015-04-03(美国)<br>\
+<br>\
+<br>\
+美国队长3! 美国队长3<br>\
+<br>\
+导演: 安东尼·罗素 / 乔·罗素<br>\
+编剧: 克里斯托弗·马库斯 / 斯蒂芬·麦克菲利 / 杰克·科比 / 乔·西蒙<br>\
+主演: 克里斯·埃文斯 / 小罗伯特·唐尼 / 斯嘉丽·约翰逊 / 塞巴斯蒂安·斯坦 / 安东尼·麦凯 / 更多...<br>\
+类型: 动作 / 科幻 / 冒险<br>\
+制片国家/地区: 美国<br>\
+语言: 科萨语 / 英语 / 德语 / 俄语 / 罗马尼亚语<br>\
+上映日期: 2016-05-06(中国大陆/美国) / 2016-04-12(加州首映)<br>\
+<br>\
+<br>\
+蜘蛛侠! 蜘蛛侠<br>\
+<br>\
+导演: 乔·沃茨<br>\
+编剧: 约翰·弗朗西斯·戴利 / 史蒂夫·迪特寇 / 乔纳森·M·戈尔茨坦 / 斯坦·李<br>\
+主演: 汤姆·赫兰德 / 小罗伯特·唐尼 / 玛丽莎·托梅 / 迈克尔·基顿 / 雅各布·巴特朗 / 更多...<br>\
+类型: 动作 / 科幻 / 冒险<br>\
+制片国家/地区: 美国<br>\
+语言: 英语<br>\
+上映日期: 2017-09-08(中国大陆) / 2017-06-28(洛杉矶首映) / 2017-07-07(美国)<br>\
+<br>\
+<br>\
+复仇者联盟2! 复仇者联盟2<br>\
+<br>\
+导演: 乔斯·韦登<br>\
+编剧: 乔斯·韦登 / 斯坦·李 / 杰克·科比<br>\
+主演: 小罗伯特·唐尼 / 克里斯·海姆斯沃斯 / 马克·鲁弗洛 / 克里斯·埃文斯 / 斯嘉丽·约翰逊 / 更多...<br>\
+类型: 动作 / 科幻 / 奇幻 / 冒险<br>\
+制片国家/地区: 美国<br>\
+语言: 英语 / 韩语<br>\
+上映日期: 2015-05-12(中国大陆) / 2015-05-01(美国)<br>\
+<br>\
+<br>\
+奇异博士! 奇异博士<br>\
+<br>\
+导演: 斯科特·德瑞克森<br>\
+编剧: 史蒂夫·迪特寇 / 托马斯·迪恩·唐纳利 / 斯坦·李 / 乔·斯派茨 / 约书亚·奥本海默<br>\
+主演: 本尼迪克特·康伯巴奇 / 蒂尔达·斯文顿 / 麦斯·米科尔森 / 切瓦特·埃加福 / 瑞秋·麦克亚当斯 / 更多...<br>\
+类型: 动作 / 奇幻 / 冒险<br>\
+制片国家/地区: 美国<br>\
+语言: 英语<br>\
+上映日期: 2016-11-04(中国大陆/美国) / 2016-10-25(英国)<br>\
+<br>\
+<br>\
+雷神2! 雷神2<br>\
+<br>\
+导演: 阿兰·泰勒<br>\
+编剧: 克里斯托弗·约斯特 / 克里斯托弗·马库斯 / 斯蒂芬·麦克菲利 / 唐·佩恩 / 罗伯特·罗达特 / 斯坦·李 / 拉里·利伯 / 杰克·科比 / 沃尔特·西蒙森<br>\
+主演: 克里斯·海姆斯沃斯 / 娜塔莉·波特曼 / 汤姆·希德勒斯顿 / 安东尼·霍普金斯 / 克里斯托弗·埃克莱斯顿 / 更多...<br>\
+类型: 动作 / 奇幻 / 冒险<br>\
+制片国家/地区: 美国<br>\
+语言: 英语<br>\
+上映日期: 2013-11-08(中国大陆/美国)<br>\
+<br>\
+<br>\
+蚁人! 蚁人<br>\
+<br>\
+导演: 佩顿·里德<br>\
+编剧: 斯坦·李 / 杰克·科比 / 拉里·利伯 / 埃德加·赖特 / 乔·考尼什 / 亚当·麦凯 / 保罗·路德<br>\
+主演: 保罗·路德 / 迈克尔·道格拉斯 / 伊万杰琳·莉莉 / 寇瑞·斯托尔 / 迈克尔·佩纳 / 更多...<br>\
+类型: 动作 / 科幻 / 冒险<br>\
+制片国家/地区: 美国<br>\
+语言: 英语<br>\
+上映日期: 2015-10-16(中国大陆) / 2015-07-17(美国)<br>\
+<br>\
+<br>\
+蚁人2! 蚁人2<br>\
+<br>\
+导演: 佩顿·里德<br>\
+编剧: 克里斯·麦克纳 / 埃里克·萨默斯 / 保罗·路德 / 安德鲁·贝伦 / 加百利·法拉利<br>\
+主演: 保罗·路德 / 伊万杰琳·莉莉 / 迈克尔·佩纳 / 汉娜·乔恩-卡门 / 沃尔顿·戈金斯 / 更多...<br>\
+类型: 动作 / 科幻 / 冒险<br>\
+制片国家/地区: 美国<br>\
+语言: 英语<br>\
+上映日期: 2018-08-24(中国大陆) / 2018-07-06(美国)<br>\
+';
+
+
+var d=dataStr.split('<br><br><br>');
+for(var i=0;i<d.length;i++){
+	var c=d[i].split('<br><br>');
+	data.push({
+	img:c[0].replace('! ',' ')+'.jpg',
+	caption:c[0].split('! ')[1],
+	desc:c[1] 
+	});
+	console.log(c[0].replace('! ',' ')+'.jpg');
+}
+
+
+
+
+ /* 4输出所有的海报*/
+ var data=data;
+ function addPhotos(){
+    
+    //var template=g('#wrap').innerHTML;
+    var template=document.getElementById('wrap').innerHTML;
+    var html=[];
+
+    //输出每个按钮，按钮对应每个海报
+
+    var nav=[];
+    for(s in data){
+      var _html=template
+                .replace("{{index}}",s)
+                .replace("{{img}}",data[s].img)
+                .replace("{{caption}}",data[s].caption)
+                .replace("{{desc}}",data[s].desc);
+      html.push(_html);
+      nav.push('<span id="nav_'+s+'" onclick="turn(document.getElementById(\'photo_'+s+'\'))" class="i"><i class="ha fa fa-share fa-2x" aria-hidden="true"></i>&nbsp;</span>');
+      
+    }
+    html.push('<div class="navbars">'+nav.join(' ')+'</div>');
+    //g('#wrap').innerHTML=html.join('');
+    document.getElementById('wrap').innerHTML=html.join(' ');
+    rsort(random([0,data.length-1]));
+   
+
+  }
+
+  addPhotos();
+
+  function  random(range){
+    var max=Math.max(range[0],range[1]);
+    var min=Math.min(range[0],range[1]);
+    var diff=max-min;
+    var number=Math.ceil(Math.random()*diff+min);
+    return number;
+
+  }
+
+   /*计算左右分区的范围*/
+   function   range(){
+    var range={ left:{x:[], y:[]} ,right:{x:[],y:[]} }
+    var wrap={
+    //  w:g('#wrap').clientWidth,
+      w:document.getElementById('wrap').clientWidth,
+      //h:g('#wrap').clientHeight
+      h:document.getElementById('wrap').clientHeight
+    }
+    var photo={
+      //w:g('.photo')[0].clientWidth,
+      w:document.getElementsByClassName('photo')[0].clientWidth,
+     // h:g('.photo')[0].clientHeight
+      h:document.getElementsByClassName('photo')[0].clientHeight
+    }
+
+    range.wrap=wrap;
+    range.photo=photo;
+    range.left.x=[ 0,wrap.w/2-photo.w/2];
+    range.left.y=[ 0,wrap.h];
+    range.right.x=[wrap.w/2+photo.w/2,wrap.w];
+    range.right.y=[0-photo.h,wrap.h];
+    console.log(wrap.h,wrap.w);
+    console.log(photo.h,photo.w);
+    return range;
+  }
+
+   /*5.左右两边排序海报*/
+   function rsort(n){
+    //var _photo=g('.photo');
+    var _photo=document.getElementsByClassName('photo');
+   // var ha=document.getElementsByClassName('i');
+    var photos=[];
+    for( var s=0;s<_photo.length;s++){
+      _photo[s].className=_photo[s].className.replace(/\s*photo_center\s*/,' ');
+     
+      _photo[s].className=_photo[s].className.replace(/\s*photo_front\s*/,' ');
+      _photo[s].className=_photo[s].className.replace(/\s*photo_back\s*/,' ');
+      _photo[s].style.left='';
+      _photo[s].style.top='';
+      _photo[s].className+='photo_front';
+      _photo[s].style['transform']='rotate(0deg) scale(1.1)';
+
+      photos.push(_photo[s]);
+    }
+
+    //var photo_center=g('#photo_'+n);
+    var photo_center=document.getElementById('photo_'+n);
+    photo_center.className+=' photo_center ';
+    photo_center=photos.splice(n,1)[0];
+
+    var photos_left=photos.splice(0,Math.ceil(photos.length/2));
+    var photos_right=photos;
+    var ranges=range();
+    for (s in photos_left){
+        var photo=photos_left[s]; 
+        photo.style.left=random(ranges.left.x)+'px';
+        photo.style.top=random(ranges.left.y)+'px';
+        photo.style['-webkit-transform']='rotate('+random([-60,60])+'deg) scale(0.8)';
+        
+    }
+    for(s in photos_right){ 
+       var photo=photos_right[s];   
+        photo.style.left=random(ranges.right.x)+'px';
+        photo.style.top=random(ranges.left.y)+'px';
+        photo.style['-webkit-transform']='rotate('+random([-60,60])+'deg) scale(0.8)';
+      }
+  
+        //翻转按钮控制
+      var navs=document.getElementsByClassName('i');
+      for ( var s=0;s<navs.length;s++){
+        navs[s].className=navs[s].className.replace(/\s*i_current\s*/,' ');
+        navs[s].className=navs[s].className.replace(/\s*i_back\s*/,' ');
+
+      }
+    
+      document.getElementById("nav_"+n).className+=' i_current ';   
+    }
+    /*1.翻面控制*/
+    function turn(elem){
+        var cls=elem.className;
+        var n=elem.id.split('_')[1];
+        
+        if( !/photo_center/.test(cls) ){
+         return rsort(n);
+        }
+        if(/photo_front/.test(cls)){
+            cls=cls.replace('photo_front','photo_back');
+            document.getElementById("nav_"+n).className+=' i_back ';
+        }else
+        {
+          cls=cls.replace('photo_back','photo_front');
+          document.getElementById('nav_'+n).className=document.getElementById('nav_'+n).className.replace(/\s*i_back\s*/,' ');
+        }
+        return elem.className=cls;
+      }
